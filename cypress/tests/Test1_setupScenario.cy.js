c131f34234e8a2462f6ee64733998aadd516214b
carlos <carlos@lepidus.com.br>
2026-05-08 12:55:52 -0400

    Adapts cypress test to OJS 3.5.0
    issue: documentacao-e-tarefas/scielo#895
    
    Signed-off-by: carlos carlos@lepidus.com.br

--- cypress/tests/Test1_setupScenario.cy.js
+++ cypress/tests/Test1_setupScenario.cy.js
@@ -13,13 +13,13 @@ describe('Setup scenario', function () {
 		});
 	});
 
-	it('install plugin doiInSummary version 1.3.1 ', function () {
-		const fileName = 'plugins/generic/pluginUpdateNotification/cypress/testData/doiInSummary.tar.gz';
+	it('install plugin shariff version 3.5.1.0 ', function () {
+		const fileName = 'plugins/generic/pluginUpdateNotification/cypress/testData/shariff-3_5_0.tar.gz';
 		cy.goToPluginSettings();
 		cy.get('.pkp_linkaction_upload').contains('Upload A New Plugin').click();
 		cy.get('#plupload-pkpUploaderDropZone').selectFile(fileName, { action: 'drag-drop' });
-		cy.get('.pkp_uploader_details').contains('doiInSummary.tar.gz');
+		cy.get('.pkp_uploader_details').contains('shariff-3_5_0.tar.gz');
 		cy.get('.submitFormButton').contains('Save').click();
-		cy.get('div').contains('Successfully installed version 1.3.1.0');
+		cy.get('div').contains('Successfully installed version 3.5.1.0');
 	});
 });
